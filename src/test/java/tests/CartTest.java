963373d78481f48e6f743b264c0c12fe5109dbdd
rhesna <rhesna1922@gmail.com>
2025-09-30 16:02:41 +0700

    add new test case

--- src/test/java/tests/CartTest.java
+++ src/test/java/tests/CartTest.java
@@ -11,63 +11,84 @@ import utils.ConfigReader;
 
 public class CartTest extends BaseTest {
 
-    //Verify Subscription in Cart page
-    @Test(priority = 1)
-    public void verifySubscriptionInCartPage(){
-        HomePage homePage = new HomePage(driver);
-        Assert.assertTrue(homePage.getHomePageValidation().contains("Automation"));
-        homePage.goToCartMenu();
+//    //Verify Subscription in Cart page
+//    @Test(priority = 1)
+//    public void verifySubscriptionInCartPage(){
+//        HomePage homePage = new HomePage(driver);
+//        Assert.assertTrue(homePage.getHomePageValidation().contains("Automation"));
+//        homePage.goToCartMenu();
+//
+//        CartPage cartPage = new CartPage(driver);
+//        cartPage.scrollToSubscribe();
+//        cartPage.enterEmailSubscription(ConfigReader.get("email"));
+//        cartPage.clickSubscription();
+//        Assert.assertTrue(cartPage.getSuccessSubscribe().contains("successfully subscribed!"));
+//    }
+//
+//    //Add Products in Cart
+//    @Test(priority = 2)
+//    public void addProductInCart(){
+//        HomePage homePage = new HomePage(driver);
+//        Assert.assertTrue(homePage.getHomePageValidation().contains("Automation"));
+//        homePage.goToProductMenu();
+//
+//        ProductPage productPage = new ProductPage(driver);
+//        productPage.scrollProduct("product1");
+//        productPage.hoverProduct("product1");
+//        productPage.addToCart("product1");
+//        productPage.clickContinueShopping();
+//        productPage.scrollProduct("product2");
+//        productPage.hoverProduct("product2");
+//        productPage.addToCart("product2");
+//        productPage.viewCart();
+//
+//        CartPage cartPage = new CartPage(driver);
+//        Assert.assertTrue(cartPage.getCartInfo().contains("Blue Top"));
+//        Assert.assertTrue(cartPage.getCartInfo().contains("Summer White Top"));
+//    }
+//
+//    //Verify Product quantity in Cart
+//    @Test(priority = 3)
+//    public void verifyProductQuantity(){
+//        HomePage homePage = new HomePage(driver);
+//        Assert.assertTrue(homePage.getHomePageValidation().contains("Automation"));
+//        homePage.goToProductMenu();
+//
+//        ProductPage productPage = new ProductPage(driver);
+//        Assert.assertEquals(productPage.getProductTitle(), "ALL PRODUCTS");
+//        productPage.displayAllProduct();
+//        productPage.scrollProduct("anyProduct");
+//        productPage.clickDetailProduct();
+//
+//        DetailProductPage detailProductPage = new DetailProductPage(driver);
+//        detailProductPage.getProductName();
+//        detailProductPage.enterQuantity("4");
+//        detailProductPage.clickCart();
+//        detailProductPage.clickViewCart();
+//
+//        CartPage cartPage = new CartPage(driver);
+//        Assert.assertEquals(cartPage.getCartQuantity(), "4");
+//    }
 
-        CartPage cartPage = new CartPage(driver);
-        cartPage.scrollToSubscribe();
-        cartPage.enterEmailSubscription(ConfigReader.get("email"));
-        cartPage.clickSubscription();
-        Assert.assertTrue(cartPage.getSuccessSubscribe().contains("successfully subscribed!"));
-    }
-
-    //Add Products in Cart
-    @Test(priority = 2)
-    public void addProductInCart(){
+    //Remove products from cart
+    @Test(priority = 4)
+    public void removeProductFromCart(){
         HomePage homePage = new HomePage(driver);
         Assert.assertTrue(homePage.getHomePageValidation().contains("Automation"));
         homePage.goToProductMenu();
+//        homePage.goToCartMenu();
 
+        //add product
         ProductPage productPage = new ProductPage(driver);
-        productPage.scrollProduct("product1");
-        productPage.hoverProduct("product1");
-        productPage.addToCart("product1");
-        productPage.clickContinueShopping();
         productPage.scrollProduct("product2");
         productPage.hoverProduct("product2");
         productPage.addToCart("product2");
         productPage.viewCart();
 
         CartPage cartPage = new CartPage(driver);
-        Assert.assertTrue(cartPage.getCartInfo().contains("Blue Top"));
-        Assert.assertTrue(cartPage.getCartInfo().contains("Summer White Top"));
-    }
-
-    //Verify Product quantity in Cart
-    @Test(priority = 3)
-    public void verifyProductQuantity(){
-        HomePage homePage = new HomePage(driver);
-        Assert.assertTrue(homePage.getHomePageValidation().contains("Automation"));
-        homePage.goToProductMenu();
-
-        ProductPage productPage = new ProductPage(driver);
-        Assert.assertEquals(productPage.getProductTitle(), "ALL PRODUCTS");
-        productPage.displayAllProduct();
-        productPage.scrollProduct("anyProduct");
-        productPage.clickDetailProduct();
-
-        DetailProductPage detailProductPage = new DetailProductPage(driver);
-        detailProductPage.getProductName();
-        detailProductPage.enterQuantity("4");
-        detailProductPage.clickCart();
-        detailProductPage.clickViewCart();
-
-        CartPage cartPage = new CartPage(driver);
-        Assert.assertEquals(cartPage.getCartQuantity(), "4");
+        Assert.assertTrue(cartPage.getCartInfo().contains("Summer"));
+        cartPage.clickRemoveProduct();
+        Assert.assertTrue(cartPage.getEmptyCartMessage().contains("empty!"));
     }
 
 }
